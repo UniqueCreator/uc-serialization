51a80e76c53dbfcc2aa7c39770ae1d910f2cb10d
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2019-02-02 11:36:20 +0200

    moved directory

---
+++ dev/include/uc/gx/lip/lip.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "base.h"
+#include "animation.h"
+#include "geo.h"
+#include "math.h"
+#include "model.h"
+#include "structs.h"
+
---
+++ dev/src/pch/pch.cpp
@@ -0,0 +1,5 @@
+#include "pch.h"
+
+#include <uc/lip/lip.h>
+#include <uc/gx/lip/lip.h>
+
