814d903472c24aaa68c8f265d1ce88d3208f7a81
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2019-02-02 11:36:41 +0200

    updated files

--- build/generate.bat
+++ build/generate.bat
@@ -3,15 +3,15 @@
 echo ^<?xml version="1.0" encoding="utf-8"?^> > build_h.msbuild
 echo ^<Project DefaultTargets="Build" ToolsVersion="15.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003"^> >> build_h.msbuild
 
-msbuild_generator.exe --input ..\src\ --mode h >> build_h.msbuild
-msbuild_generator.exe --input ..\include\ --mode h >> build_h.msbuild
+msbuild_generator.exe --input ..\dev\src\ --mode h >> build_h.msbuild
+msbuild_generator.exe --input ..\dev\include\ --mode h >> build_h.msbuild
 
 echo ^</Project^> >> build_h.msbuild
 
 echo ^<?xml version="1.0" encoding="utf-8"?^> > build_cpp.msbuild
 echo ^<Project DefaultTargets="Build" ToolsVersion="15.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003"^> >> build_cpp.msbuild
 
-msbuild_generator.exe --input ..\src\ --mode cpp >> build_cpp.msbuild
+msbuild_generator.exe --input ..\dev\src\ --mode cpp >> build_cpp.msbuild
 
 echo ^</Project^> >> build_cpp.msbuild
 
@@ -19,9 +19,9 @@ echo ^</Project^> >> build_cpp.msbuild
 echo ^<?xml version="1.0" encoding="utf-8"?^> > build.vcxproj.filters
 echo ^<Project DefaultTargets="Build" ToolsVersion="15.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003"^> >> build.vcxproj.filters
 
-msbuild_generator.exe --type filters --input ..\src\ --mode h >> build.vcxproj.filters
-msbuild_generator.exe --type filters --input ..\src\ --mode cpp >> build.vcxproj.filters
-msbuild_generator.exe --type filters --input ..\include\ --mode h >> build.vcxproj.filters
+msbuild_generator.exe --type filters --input ..\dev\src\ --mode h >> build.vcxproj.filters
+msbuild_generator.exe --type filters --input ..\dev\src\ --mode cpp >> build.vcxproj.filters
+msbuild_generator.exe --type filters --input ..\dev\include\ --mode h >> build.vcxproj.filters
 
 echo ^</Project^> >> build.vcxproj.filters
 
